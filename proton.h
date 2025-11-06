2b98990dee6faa0e0d9bef7774e40bb5443f04a7
Glenn Fiedler <glenn@networknext.com>
2025-11-06 10:29:37 -0500

    nope it has to be GPL to link to stuff we need

--- proton.h
+++ proton.h
@@ -3,6 +3,8 @@
 
     Tested on Ubuntu 22.04 LTS and Ubuntu 24.04 LTS
 
+    Licensed under the GNU General Public License 3.0
+
     USAGE:
 
         sudo insmod proton.ko
@@ -14,24 +16,6 @@
 
         sudo bpftool btf show
         sudo bpftool btf dump id <id>
-
-    ISC License:
-
-        Copyright (c) 2025 Network Next, Inc.
-
-        Written by Glenn Fiedler <glenn@networknext.com>
-
-        Permission to use, copy, modify, and/or distribute this software for any
-        purpose with or without fee is hereby granted, provided that the above
-        copyright notice and this permission notice appear in all copies.
-        
-        THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-        WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-        MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-        ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-        WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-        ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-        OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
 #pragma once

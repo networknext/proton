3367fac86b7bd415a4f4af6e46ba7842f8435793
Glenn Fiedler <glenn@networknext.com>
2025-11-06 10:20:45 -0500

    ISC license is more appropriate I think

--- proton.c
+++ proton.c
@@ -1,7 +1,5 @@
 /*
-    Proton Linux kernel module. Copyright 2017 - 2025 Network Next, Inc.
-
-    Licensed under the GNU General Public License 3.0
+    Proton Linux kernel module
 
     Tested on Ubuntu 22.04 LTS and Ubuntu 24.04 LTS
 
@@ -16,6 +14,24 @@
 
         sudo bpftool btf show
         sudo bpftool btf dump id <id>
+
+    ISC License:
+
+        Copyright 2017 - 2025 Network Next, Inc.
+
+        Written by Glenn Fiedler <glenn@networknext.com>
+
+        Permission to use, copy, modify, and/or distribute this software for any
+        purpose with or without fee is hereby granted, provided that the above
+        copyright notice and this permission notice appear in all copies.
+        
+        THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+        WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+        MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+        ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+        WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+        ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+        OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
 #include "proton.h"
@@ -140,6 +156,6 @@ module_exit( proton_exit );
 #include "hydrogen.c"
 
 MODULE_VERSION( "1.0.0" );
-MODULE_LICENSE( "GPL" ); 
+MODULE_LICENSE( "ISC" ); 
 MODULE_AUTHOR( "Glenn Fiedler" ); 
 MODULE_DESCRIPTION( "Proton kernel module. Provides crypto functions that are callable from XDP programs." );
